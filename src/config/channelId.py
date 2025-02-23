38f3d3db13c4cfa12ff3efdf4b0599e8d9ab5f51
kimjunhee <kimjunhee2483@gmail.com>
2025-02-23 19:25:02 +0900

    02/23-config

--- src/config/channelId.py
+++ src/config/channelId.py
@@ -1,6 +1,7 @@
 CHANNELID = {
-    "youtuber1" : "youtuber1 Channel Id",
-    "youtuber2" : "youtuber2 Channel Id",
-    "youtuber3" : "youtuber3 Channel Id",
-    "youtuber4" : "youtuber4 Channel Id"
+    "우정잉" : "UCW945UjEs6Jm3rVNvPEALdg",
+    "청산유수현 SUHYEON " : "UCnLtTiIMVNpsv3laNBTK53w",
+    "보겸TV" : "UCu9BCtGIEr73LXZsKmoujKw",
+    "지식줄고양" : "UCPDKAhacqpqmP7aXWem3_Jg",
+    "김 럽미" : "UCABP4GiFpNSlsJYDEZ_Wysw"
 }
